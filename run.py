6f0c3f61673932e20d0a420920f0084fbf907206
kdeane87 <kdeane87@gmail.com>
2023-06-09 10:30:43 +0000

    add updte surplus data function

--- run.py
+++ run.py
@@ -66,6 +66,18 @@ def update_sales_worksheet(data):
     print('Sales worksheet updated successfully.\n')
 
 
+def update_surplus_worksheet(new_surplus_data):
+    """
+    update surplus worksheet
+    """
+    print('updating surplus data')
+    surplus_worksheet = SHEET.worksheet('surplus')
+    surplus_worksheet.append_row(new_surplus_data)
+    print('Surplus worksheet updated successfully.\n')
+
+
+
+
 def calculate_surplus_data(sales_row):
     """
     Compare sales with stock and calculate the surplus for each item type.
@@ -97,7 +109,7 @@ data = get_sales_data()
 sales_data = [int(num) for num in data]
 update_sales_worksheet(sales_data)
 new_surplus_data = calculate_surplus_data(sales_data)
-print(new_surplus_data)
+update_surplus_worksheet(new_surplus_data)
 
 
 print('Welcome to love sandwiches data automation')
